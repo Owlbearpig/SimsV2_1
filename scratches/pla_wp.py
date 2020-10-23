34e952ba4c759813956ae68d49b0c41caff7da6f
Owlbearpig <alexj07@gmail.com>
2020-10-23 19:31:40 +0200

    - Some wpa1_table1 plot

--- scratches/pla_wp.py
+++ scratches/pla_wp.py
@@ -5,7 +5,8 @@ from modules.utils.constants import plot_data_dir
 import pandas
 
 
-data_dir = Path('/home/alex/Desktop/MDrive/AG/BFWaveplates/Data/PLAWP')
+#data_dir = Path('/home/alex/Desktop/MDrive/AG/BFWaveplates/Data/PLAWP_table1')
+data_dir = Path('E:\MEGA\AG\BFWaveplates\Data\PLAWP_table1')
 files = list(data_dir.iterdir())
 
 def fft(data):
@@ -17,21 +18,48 @@ def fft(data):
 
     return freqs[idx], Y[idx]
 
-ref0 = fft(np.loadtxt(data_dir / files[0]))[1]
+fft_ref = fft(np.loadtxt(data_dir / files[0]))
+
+freqs = fft_ref[0]
+#print(freqs)
+# 6
 
 for file in files:
+    file = str(file)
+    if 'reference' in file:
+        continue
     data = np.loadtxt(data_dir / file)
-    ft = fft(data)
-    plt.plot(ft[0], 20*np.log10(np.abs(ft[1])/np.abs(ref0)), label=file)
-    """
-    df = pandas.DataFrame({'x': data[:, 0],
-                           'y': data[:, 1]})
-    df.to_csv(plot_data_dir / 'fft' / (file.stem + '.csv'), index_label='n')
-    """
-    #plt.plot(ft[0], ft[1], label=file)
-    plt.xlim([0.0, 0.6])
+    fft_file = fft(data)[1]
 
-plt.xlabel('Frequency (THz)')
-plt.ylabel('Intensity (dB)')
-plt.legend()
+    plt.plot(freqs, 20*np.log10(np.abs(fft_file)/np.abs(fft_ref[1])))
+
+plt.xlim((0, 1.5))
+plt.ylim((-130, 10))
+plt.show()
+
+
+freq = 0.4
+freq_index = np.argmin(np.abs(freqs-freq))
+print(freqs[freq_index], freq_index)
+
+ft_list, degress = [], []
+for file in files:
+    file = str(file)
+    if 'reference' in file:
+        continue
+
+    deg = float(file.split('-')[-1].split(' ')[0])
+    data = np.loadtxt(data_dir / file)
+    ft = np.abs(fft(data)[1][freq_index])/np.abs(fft_ref[1][freq_index])
+    ft_list.append(ft)
+    degress.append(deg)
+    #plt.plot(ft[0], 20*np.log10(np.abs(ft[1])/np.abs(ref0)), label=file)
+
+    #plt.plot(ft[0], ft[1], label=file)
+    #plt.xlim([0.0, 0.6])
+
+plt.plot(degress, ft_list)
+plt.xlabel('Angle (Deg)')
+plt.ylabel('Transmission')
+#plt.legend()
 plt.show()
